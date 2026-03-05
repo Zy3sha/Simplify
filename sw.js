f4f338f96a7bce1e8bb6b974c1414a8cbbf72636
Zy3sha <zy3sha@gmail.com>
2026-03-05 22:51:08 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+const cacheName="oliver-tracker-v1"
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(cacheName).then(cache=>{
+return cache.addAll([
+"/",
+"/index.html",
+"/icon.png"
+])
+})
+)
+})
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request)
+})
+)
+})
