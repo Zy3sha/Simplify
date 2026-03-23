85643db8115567d1092cdaf7a9b3c8a7875e368a
Obaby <zy3sha@gmail.com>
2026-03-23 09:50:40 +0000

    Add files via upload

--- app.jsx
+++ app.jsx
@@ -18346,4 +18346,4 @@ Severe (anaphylaxis): breathing difficulty, swelling of face/throat, pale/floppy
     </div>
   );
 }
-createRoot(document.getElementById('root')).render(React.createElement(ErrorBoundary,null,React.createElement(App)));
+ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(ErrorBoundary,null,React.createElement(App)));
